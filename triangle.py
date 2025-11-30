fb08865587b139953525cc90c156f45c10dec246
razibdash <avrorazib@gmail.com>
2025-11-30 23:07:24 +0600

    add triangle and square

---
+++ triangle.py
@@ -0,0 +1,81 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+# -----------------------------
+# Draw X and Y axes
+# -----------------------------
+def draw_axes():
+    glColor3f(1.0, 1.0, 1.0)       # White
+    glLineWidth(1)
+
+    glBegin(GL_LINES)
+    # X-axis
+    glVertex2f(-10, 0)
+    glVertex2f(10, 0)
+
+    # Y-axis
+    glVertex2f(0, -10)
+    glVertex2f(0, 10)
+    glEnd()
+
+
+# -----------------------------
+# Draw triangle
+# -----------------------------
+def draw_triangle():
+    glColor3f(0.0, 1.0, 0.0)       # Green
+    glLineWidth(3)
+
+    glBegin(GL_LINE_LOOP)
+    glVertex2f(3, 8)  
+    glVertex2f(6, 2)
+    glVertex2f(1, 2)
+    glEnd()
+
+
+# -----------------------------
+# Main display function
+# -----------------------------
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+    draw_axes()
+    draw_triangle()
+
+    glFlush()
+    glutSwapBuffers()
+
+
+# -----------------------------
+# Window Reshape
+# -----------------------------
+def reshape(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+
+    # Orthographic projection
+    glOrtho(-10, 10, -10, 10, -1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+
+
+# -----------------------------
+# GLUT Initialization
+# -----------------------------
+glutInit()
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+glutInitWindowSize(500, 500)
+glutCreateWindow(b"OpenGL Triangle & Axes")
+
+# Background color
+glClearColor(0.0, 0.0, 0.0, 1.0)
+
+glEnable(GL_DEPTH_TEST)
+
+glutDisplayFunc(display)
+glutReshapeFunc(reshape)
+
+glutMainLoop()
